1364d3dce5fc05ae06bbeb8e55a10b4bf764dadd
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2022-10-14 20:22:09 +0700

    create login activity

---
+++ app/src/main/java/com/example/modul6/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.modul6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText etEmail;
+    EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.buttonSignUp);
+        etEmail = findViewById(R.id.email);
+        etPassword = findViewById(R.id.password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                if (email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Email dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Berhasil Register", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+    }
+}
